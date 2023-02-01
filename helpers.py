8ccaa9c9305a19417bef05724300a197eb6e02e5
Ykyki4 <jimmypantalones@gmail.com>
2023-02-01 22:42:44 +0300

    Refactor code, split by reusable functions.

---
+++ helpers.py
@@ -0,0 +1,55 @@
+import logging
+import json
+
+
+async def send_message(writer, message):
+    message = message.encode(encoding='utf-8')+b'\n'
+    writer.write(message)
+    await writer.drain()
+    logging.debug(f'message {message} has been sent')
+
+
+async def register(reader, writer, username):
+    data = await reader.readline()
+    logging.debug(data.decode())
+
+    await send_message(writer, '')
+
+    data = await reader.readline()
+    logging.debug(data.decode())
+
+    await send_message(writer, username)
+
+    data = await reader.readline()
+    response = json.loads(data.decode())
+    user_hash = response.get('account_hash')
+    nickname = response.get('nickname')
+    user_data = f'Name: {nickname}, hash: {user_hash}'
+    logging.debug(f'Registred. '+user_data)
+
+    with open(f'{nickname}.txt', 'w', encoding='utf-8') as f:
+        f.write(user_data)
+
+    writer.close()
+
+    return user_hash
+
+
+async def authorize(reader, writer, user_hash, message):
+    data = await reader.readline()
+    logging.debug(data.decode())
+
+    await send_message(writer, user_hash)
+
+    data = await reader.readline()
+    if json.loads(data) is None:
+        logging.error('Invalid token. Check it out or register again.')
+        writer.close()
+        return
+    response = json.loads(data.decode())
+    logging.debug(f'Authorized. Name: {response.get("nickname")}, hash: {response.get("account_hash")}')
+
+    # Именно для отправки сообщения в чат требуется добавить два '\n', в остальных случаях всё нормально с одним.
+    await send_message(writer, message + '\n')
+
+    writer.close()
--- sender.py
+++ sender.py
@@ -1,10 +1,10 @@
 import asyncio
 import argparse
-import json
-import sys
 import logging
 
 from environs import Env
+
+from helpers import authorize, register,
 
 
 HOST = 'minechat.dvmn.org'
@@ -14,40 +14,27 @@ PORT = 5050
 def arg_parser(host, port):
     parser = argparse.ArgumentParser(prog='Script to send messages into minechat')
 
-    parser.add_argument('user_hash', type=str, help='Your user hash')
     parser.add_argument('message', type=str, help='Message to send')
+    parser.add_argument('-u', '--username', type=str, help='Username you want to use')
+    parser.add_argument('-ha', '--hash', type=str, help='Your user hash')
     parser.add_argument('-ho', '--host', type=str, help='Host of minechat', default=host)
     parser.add_argument('-p', '--port', type=int, help='Port of minechat', default=port)
 
     return parser.parse_args()
 
 
-async def send_message(writer, message):
-    message = message.encode(encoding="utf-8") + b'\n\n'
-    writer.write(message)
-    await writer.drain()
-    logging.debug(f'message {message} has been sent')
-
-
-async def get_messenger_connection(host, port, message, user_hash):
+async def get_messenger_connection(args):
     reader, writer = await asyncio.open_connection(
-        host, port)
+        args.host, args.port)
 
-    data = await reader.readline()
-    logging.debug(data.decode())
+    if args.hash:
+        await authorize(reader, writer, args.hash, args.message)
 
-    await send_message(writer, user_hash)
-
-    data = await reader.readline()
-    if json.loads(data) is None:
-        logging.debug('Invalid token. Check it out or register again.')
-        writer.close()
-        return 
-    logging.debug(data.decode())
-
-    await send_message(writer, message)
-
-    writer.close()
+    if args.username:
+        user_hash = await register(reader, writer, args.username)
+        reader, writer = await asyncio.open_connection(
+            args.host, args.port)
+        await authorize(reader, writer, user_hash, args.message)
 
 
 if __name__ == '__main__':
@@ -57,4 +44,4 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     args = arg_parser(env('HOST', HOST), env('PORT', PORT))
-    asyncio.run(get_messenger_connection(args.host, args.port, args.message, args.user_hash))
+    asyncio.run(get_messenger_connection(args))
