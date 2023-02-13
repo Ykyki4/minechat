ce4d225a9943ecc11e9453871fb9a5d71a87856e
Ykyki4 <jimmypantalones@gmail.com>
2023-02-13 11:44:51 +0300

    change var name data to received_line

--- helpers.py
+++ helpers.py
@@ -14,25 +14,25 @@ async def send_message(writer, message):
 
 
 async def register(reader, writer, nickname):
-    data = await reader.readline()
-    logging.debug(data.decode())
+    received_line = await reader.readline()
+    logging.debug(received_line.decode())
 
     await send_message(writer, '')
 
-    data = await reader.readline()
-    logging.debug(data.decode())
+    received_line = await reader.readline()
+    logging.debug(received_line.decode())
 
     await send_message(writer, sanitize_message(nickname))
 
-    data = await reader.readline()
-    response = json.loads(data.decode())
+    received_line = await reader.readline()
+    response = json.loads(received_line.decode())
     user_hash = response.get('account_hash')
     nickname = response.get('nickname')
-    user_data = f'Name: {nickname}, hash: {user_hash}'
-    logging.debug(f'Registred. '+user_data)
+    user_received_line = f'Name: {nickname}, hash: {user_hash}'
+    logging.debug(f'Registred. '+user_received_line)
 
     with open(f'{nickname}.txt', 'w', encoding='utf-8') as f:
-        f.write(user_data)
+        f.write(user_received_line)
 
     writer.close()
 
@@ -40,17 +40,17 @@ async def register(reader, writer, nickname):
 
 
 async def authorize(reader, writer, user_hash, message):
-    data = await reader.readline()
-    logging.debug(data.decode())
+    received_line = await reader.readline()
+    logging.debug(received_line.decode())
 
     await send_message(writer, sanitize_message(user_hash))
 
-    data = await reader.readline()
-    if json.loads(data) is None:
+    received_line = await reader.readline()
+    if json.loads(received_line) is None:
         logging.error('Invalid token. Check it out or register again.')
         writer.close()
         return
-    response = json.loads(data.decode())
+    response = json.loads(received_line.decode())
     logging.debug(f'Authorized. Name: {response.get("nickname")}, hash: {response.get("account_hash")}')
 
     # Именно для отправки сообщения в чат требуется добавить два '\n', в остальных случаях всё нормально с одним.
